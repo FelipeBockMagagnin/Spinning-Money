854c48cb9bdfd8956c357bb704fe003e767111a7
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2019-02-22 16:42:18 -0300

    0.03

---
+++ Spinning Money/Assets/Scripts/MarketManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Item
+{
+    public string name;
+    public int price;
+    //[HideInInspector]
+    //public bool blocked;
+    public Button button;
+    public int IncreseMultiplyAmount;
+    //public Button PriceButton;
+}
+
+public class MarketManager : MonoBehaviour
+{
+    public Item[] items;
+    InputManager inputManager;
+    MarketManager instance;
+
+    private void Awake()
+    {
+        inputManager = GameObject.Find("Cube").GetComponent<InputManager>();
+    }
+
+
+    /// <summary>
+    /// Compra um determinado estilo
+    /// </summary>
+    /// <param name="item"></param>
+    void ButtonBuy(Item item)
+    {
+        Debug.Log("item " + item.name + " desbloqueado");
+        inputManager.multiplicator += item.IncreseMultiplyAmount;
+    }
+
+    /// <summary>
+    /// Compra um novo estilo/musica
+    /// </summary>
+    /// <param name="name"></param>
+    public void Buy(string name)
+    {
+        foreach (Item item in items)
+        {
+            if (item.name == name)
+            {
+                if (inputManager.money  >= item.price)
+                {
+                    Debug.Log("Item " + item.name + " comprado com sucesso");
+                    ButtonBuy(item);
+                    inputManager.money -= item.price;
+                }
+                else
+                {
+                    Debug.Log("Não há dinheiro para comprar o Item " + item.name);
+                }
+
+            }
+        }
+    }
+}
