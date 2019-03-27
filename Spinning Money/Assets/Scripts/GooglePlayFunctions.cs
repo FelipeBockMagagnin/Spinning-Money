5607443d370e1f887c3d679b61984059eb12a3ab
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2019-03-27 15:21:10 -0300

    Implemented google login

---
+++ Spinning Money/Assets/Scripts/GooglePlayFunctions.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GooglePlayFunctions : MonoBehaviour
+{
+
+    public Text signInButtonText;
+    public Text authStatus;
+
+    private void Start()
+    {
+        // Create client configuration
+        PlayGamesClientConfiguration config = new
+            PlayGamesClientConfiguration.Builder()
+            .Build();
+
+        // Enable debugging output (recommended)
+        PlayGamesPlatform.DebugLogEnabled = true;
+
+        // Initialize and activate the platform
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+        // END THE CODE TO PASTE INTO START
+
+        PlayGamesPlatform.Instance.Authenticate(SignInCallback, true);
+    }
+
+    public void SignIn()
+    {
+        if (!PlayGamesPlatform.Instance.localUser.authenticated)
+        {
+            // Sign in with Play Game Services, showing the consent dialog
+            // by setting the second parameter to isSilent=false.
+            PlayGamesPlatform.Instance.Authenticate(SignInCallback, false);
+        }
+        else
+        {
+            // Sign out of play games
+            PlayGamesPlatform.Instance.SignOut();
+
+            // Reset UI
+            signInButtonText.text = "Sign In";
+            authStatus.text = "";
+        }
+    }
+
+    public void SignInCallback(bool success)
+    {
+        if (success)
+        {
+            Debug.Log("(Lollygagger) Signed in!");
+
+            // Change sign-in button text
+            signInButtonText.text = "Sign out";
+
+            // Show the user's name
+            authStatus.text = "Signed in as: " + Social.localUser.userName;
+        }
+        else
+        {
+            Debug.Log("(Lollygagger) Sign-in failed...");
+
+            // Show failure message
+            signInButtonText.text = "Sign in";
+            authStatus.text = "Sign-in failed";
+        }
+    }
+
+
+}
