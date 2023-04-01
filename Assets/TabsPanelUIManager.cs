5de75784c805cd95dda6727b51c0ded98024e873
felipe.magagnin <felipebmagagnin@gmail.com>
2023-04-01 17:29:36 -0300

    Improve UI and move all folders to root

---
+++ Assets/TabsPanelUIManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabsPanelUIManager : MonoBehaviour
+{
+    public GameObject upgradesPanel;
+    public GameObject investorsPanel;
+    public GameObject utilitiesPanel;
+
+    void Start()
+    {
+        upgradesPanel.SetActive(true);
+        investorsPanel.SetActive(false);
+        utilitiesPanel.SetActive(false);
+    }
+
+    public void ShowUpgradesPanel() {
+        upgradesPanel.SetActive(true);
+        investorsPanel.SetActive(false);
+        utilitiesPanel.SetActive(false);
+    }
+
+    public void ShowInvestorsPanel() {
+        upgradesPanel.SetActive(false);
+        investorsPanel.SetActive(true);
+        utilitiesPanel.SetActive(false);
+    }
+
+    public void ShowUtilitiesPanel() {
+        upgradesPanel.SetActive(false);
+        investorsPanel.SetActive(false);
+        utilitiesPanel.SetActive(true);
+    }
+}
